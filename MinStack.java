3eaaa212a1f9ff179c4e6c5e8cf4f3ccd5306b81
Rashmi Kumari <44300603+RashmiKumari21@users.noreply.github.com>
2021-12-09 22:47:06 -0800

    Update MinStack.java

--- MinStack.java
+++ MinStack.java
@@ -42,3 +42,61 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
+
+
+
+//Approach 2
+//TC O(1)
+
+
+class MinStack {
+    Stack<Integer> st;
+    Stack<Integer> st2;
+    int min;
+    
+    public MinStack() {
+        st = new Stack<Integer>();
+        st2= new Stack<Integer>();
+        min = Integer.MAX_VALUE;     
+        st2.push(min);
+    }
+    
+    public void push(int val) {
+        if (val <= min){
+            st.push(val); 
+            st2.push(val);
+            min= val;
+        }
+        else 
+            
+        {
+            st.push(val);
+            st2.push(min);
+        }
+    }
+    
+    public void pop() {
+        st.pop();
+        st2.pop();
+        min=st2.peek();
+    }
+    
+    public int top() {      
+        return st.peek();       
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
